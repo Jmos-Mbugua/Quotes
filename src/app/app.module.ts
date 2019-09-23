742989c1ffe22cec8bfdc9ac94211616e3f57323
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-09-23 12:02:13 +0300

    Adds Forms component to app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesNavbarComponent } from './quotes-navbar/quotes-navbar.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { DatecountPipe } from './datecount.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesNavbarComponent,
+    QuotesNavbarComponent,    
+    QuotesFormComponent, DatecountPipe,
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
